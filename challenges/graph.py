7f3c9d425202c5b49b996bfc408881efecd71069
C3NZ <cenz@cenz.io>
2019-07-14 20:57:38 -0700

    [feat] implement the graph and vertex class

---
+++ challenges/graph.py
@@ -0,0 +1,113 @@
+class Vertex:
+    def __init__(self, key: str):
+        self.key = key
+        self.__neighbors = []
+
+    def __eq__(self, otherVert):
+        return self.key == otherVert.key
+
+    def __hash__(self):
+        return hash(self.key)
+
+    def __str__(self):
+        return "V-" + str(self.key)
+
+    def __inNeighbors(self, vert):
+        """
+            Check if the vertex is already a neighbor to this current one 
+        """
+        if not self.__neighbors:
+            return False
+
+        for storedVert, _ in self.__neighbors:
+            if vert == storedVert:
+                return True
+
+        return False
+
+    @property
+    def neighbors(self):
+        """
+            Get the neighbors of the vertex
+        """
+        return self.__neighbors
+
+    def addNeighbor(self, vert: tuple):
+        """
+            Add a neighbor to this vertex
+        """
+        if not self.__inNeighbors(vert):
+            self.__neighbors.append(vert)
+
+
+class Graph:
+    def __init__(self):
+        self.graph = {}
+        self.verticies = 0
+        self.edges = 0
+
+    def addVertex(self, vertex: Vertex):
+        """
+            Add a vertex to the graph
+
+            Args:
+                vertex - The vertex object that we would like to be adding.
+        """
+        if vertex not in self.graph:
+            self.graph[vertex] = vertex
+            self.verticies += 1
+
+        raise KeyError("The Vertex you're trying to add already exists")
+
+    def getVertex(self, vertKey: int):
+        """
+            Get a specific vertex from the set of verticies we have.
+
+            Args:
+                vertKey - the integer of the vert key we're looking for
+
+            Returns:
+                a vertex object if the vertkey is found
+        """
+        if vertKey in self.graph:
+            return self.graph[vertKey]
+
+        raise KeyError("The Vertex is not stored within this graph.")
+
+    def getVerticies(self):
+        """
+            Return a list of all the vertex objects
+
+            Returns:
+                a list of all verticies objects within the graph
+        """
+        return self.graph.values()
+
+    def addEdge(self, fromVert: Vertex, toVert: Vertex, weight: float = 0):
+        """
+           Add an edge to the graph 
+
+           Args:
+               fromVert - The vertex object we're connecting the toVert to
+               toVert - The vertex object we're connecting the fromVert to
+               weight - (0) - The weight of the edge 
+        """
+        if fromVert not in self.graph or toVert not in self.graph:
+            raise ValueError("One of the verticies is not currently in the graph.")
+        elif fromVert == toVert:
+            raise ValueError("You cannot have a vertex connect to itself.")
+
+        self.graph[fromVert].addNeighbor((toVert, weight))
+        self.graph[toVert].addNeighbor((fromVert, weight))
+
+    def getNeighbors(self, vert: Vertex):
+        """
+            Get the neighbors of a vertex stored within the graph.
+
+            Args:
+                vert: The vertex we're trying to get the neighbors of.
+        """
+        if vert not in self.graph:
+            raise KeyError("The vertex is not in the graph")
+
+        return self.graph[vert].neighbors
