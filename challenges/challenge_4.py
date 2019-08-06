ef4ea8a5a40fc1ac354832806bb532f0d56d10ac
C3NZ <cenz@cenz.io>
2019-08-05 17:48:07 -0700

    [add] the fixed memoized version of the knapsack problem

--- challenges/challenge_4.py
+++ challenges/challenge_4.py
@@ -15,6 +15,9 @@ class Item:
         self.value = value
         self.weight = weight
 
+    def __hash__(self):
+        return hash(self.name)
+
     def __eq__(self, other_item):
         return self.value == other_item.value
 
@@ -28,7 +31,7 @@ class Item:
         return f"<Item: {self.name}, Value: {self.value}, Weight: {self.weight}>"
 
     def __repr__(self):
-        return f"<Item: {self.name}, Value: {self.value}, Weight: {self.weight}>"
+        return str(self)
 
 
 def simple_knapsack(capacity, items, cost_func, curr_index=None):
@@ -42,7 +45,7 @@ def simple_knapsack(capacity, items, cost_func, curr_index=None):
     if curr_index is None:
         curr_index = len(items) - 1
 
-    if capacity == 0 or curr_index <= 0:
+    if capacity == 0 or curr_index < 0:
         return 0, []
 
     curr_item: Item = items[curr_index]
@@ -77,32 +80,115 @@ def simple_knapsack(capacity, items, cost_func, curr_index=None):
     return value_without_item, items_without_curr
 
 
-def main():
+def memoized_knapsack(capacity, items, cost_func, curr_index=None, memo=None):
+    """
+        Memoized knapsack solution
+    """
+    # If there is no more space left or we've traversed all the items, return
+    # 0, as we can't go further
+
+    if curr_index is None:
+        curr_index = len(items) - 1
+        memo = {}
+
+    if capacity == 0 or curr_index < 0:
+        return 0, []
+
+    curr_item: Item = items[curr_index]
+
+    if curr_item.weight > capacity:
+        return memoized_knapsack(capacity, items, cost_func, curr_index - 1, memo)
+
+    # Get the item value and calculate the new capacity of our knapsack
+    # after an item is added.
+    item_value = cost_func(curr_item)
+    new_capacity = capacity - curr_item.weight
+
+    # Check if
+    if curr_index not in memo:
+        value_with_item, items_with_curr = memoized_knapsack(
+            new_capacity, items, cost_func, curr_index - 1, memo
+        )
+
+        total_value_with_item = value_with_item + item_value
+        memo[curr_index] = total_value_with_item, items_with_curr
+    else:
+        value_with_item, items_with_curr = memo[curr_index]
+
+    total_value_with_item = value_with_item + item_value
+
+    if curr_index - 1 not in memo:
+        # Don't take the item, but take the value
+        value_without_item, items_without_curr = memoized_knapsack(
+            capacity, items, cost_func, curr_index - 1, memo
+        )
+
+        memo[curr_index - 1] = value_without_item, items_without_curr
+    else:
+        value_without_item, items_without_curr = memo[curr_index - 1]
+
+    # If the value with the item is greater than the value without the
+    # item, return the current total value and all the items
+    if total_value_with_item > value_without_item:
+        all_items = [curr_item]
+        all_items.extend(items_with_curr)
+        return total_value_with_item, all_items
+
+    return value_without_item, items_without_curr
+
+
+def main(knapsack_items):
     """
         Solve the knapsack problem and another DP programming challenge.
     """
     capacity = 20
-    items = [
-        Item("Macbook", 20, 15),
-        Item("TV", 15, 30),
-        Item("IPhone", 5, 8),
-        Item("USB", 3, 1),
-        Item("Minecraft", 20, 1),
-        Item("Super dense diamond", 50, 30),
-        Item("Toothbrush", 1, 1),
-        Item("Monitor", 5, 5),
-        Item("Shoes", 2, 5),
-        Item("Books", 4, 20),
-    ]
 
-    items = sorted(items)
+    knapsack_items = sorted(knapsack_items)
 
-    item_values, items = simple_knapsack(capacity, items, lambda item: item.value)
+    item_score, items = simple_knapsack(
+        capacity, knapsack_items, lambda item: item.value
+    )
 
     item_values, items = simple_knapsack(
-        capacity, items, lambda item: item.value / item.weight
+        capacity, knapsack_items, lambda item: item.value / item.weight
+    )
+
+    print(simple_knapsack(capacity, knapsack_items, lambda item: item.value))
+    print(memoized_knapsack(capacity, knapsack_items, lambda item: item.value))
+
+    print(
+        timeit.timeit(
+            "simple_knapsack(40, sorted(knapsack_items), lambda item: item.value)",
+            setup="from challenge_4 import simple_knapsack, knapsack_items",
+            number=10,
+        )
+    )
+
+    print(
+        timeit.timeit(
+            "memoized_knapsack(40, sorted(knapsack_items), lambda item: item.value)",
+            setup="from challenge_4 import memoized_knapsack, knapsack_items",
+            number=10,
+        )
     )
 
 
+import timeit
+
+knapsack_items = [
+    Item("Macbook", 10, 15),
+    Item("TV", 20, 30),
+    Item("IPhone", 5, 8),
+    Item("USB", 3, 1),
+    Item("Minecraft", 1, 20),
+    Item("Super dense diamond", 50, 30),
+    Item("Toothbrush", 1, 1),
+    Item("Monitor", 5, 5),
+    Item("Shoes", 2, 5),
+    Item("Books", 4, 5),
+    Item("Mcdonalds", 1, 5),
+    Item("Yeezys", 2, 20),
+]
 if __name__ == "__main__":
-    main()
+
+    main(knapsack_items)
